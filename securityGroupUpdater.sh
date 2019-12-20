47e069d0dafeaff431d7db88281d2bb25b63a259
Evan Day <evan@evanday.dev>
2019-12-20 19:07:46 +0000

    added the remaining commands needed

--- securityGroupUpdater.sh
+++ securityGroupUpdater.sh
@@ -1,2 +1,6 @@
 sourceIPAddress=$(curl -s ifconfig.me)
-echo "The IP Address to add to your Security Group is $sourceIPAddress"
+formattedAddress="${sourceIPAddress}/32"
+echo "The IP Address to add to your Security Group is $formattedAddress"
+read -p "Enter the group ID of the Security Group to update: " securityGroupID
+echo "Updating the specified Security Group. This will fail if the rule already exists but we dont need to worry about that"
+aws ec2 authorize-security-group-ingress --group-id $securityGroupID  --protocol tcp --port 22 --cidr $formattedAddress
